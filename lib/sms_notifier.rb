17fd2c85b924cb596e0601c800aa5eacac0114ac
Caroline Peri <caroline.peri@planningcenter.com>
2026-08-20 10:21:31 -0400

    build out messages in SMS and Email notifier classes

--- lib/sms_notifier.rb
+++ lib/sms_notifier.rb
@@ -9,4 +9,19 @@ class SmsNotifier
     message = "You're registered for #{event[:name]}!"
     notifications << "SMS: #{attendee[:phone]} - #{message}"
   end
+
+  def send_waitlist(event, attendee)
+    message = "Waitlisted for #{event[:name]}."
+    notifications << "SMS: #{attendee[:phone]} - #{message}"
+  end
+
+  def send_cancellation(event, attendee, cancellation_message)
+    message = "Cancelled: #{event[:name]}."
+    notifications << "SMS: #{attendee[:phone]} - #{message}"
+  end
+
+  def send_promotion(event, attendee, price)
+    message = "Promoted from waitlist: #{event[:name]}!"
+    notifications << "SMS: #{attendee[:phone]} - #{message}"
+  end
 end
